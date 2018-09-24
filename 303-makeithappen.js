d5f09a4e8b0e7174bf676e930cf183b35d17e787
Heta Pärssinen <heta.parssinen@gmail.com>
2018-09-24 20:36:40 +0300

    Check that input is not empty, reverse list order, empty input value after submit

---
+++ 303-makeithappen.js
@@ -0,0 +1,84 @@
+function addNewTodo(name) {
+  if (name !== '') {
+    var newTodoKey = firebase
+      .database()
+      .ref()
+      .child('todos')
+      .push().key;
+    updateTodo(newTodoKey, name, false);
+  }
+}
+
+function readTodos() {
+  firebase
+    .database()
+    .ref('todos')
+    .on('value', updateDOM);
+}
+
+function updateTodo(key, name, done) {
+  if (key) {
+    firebase
+      .database()
+      .ref('todos/' + key)
+      .set({
+        name: name,
+        done: done,
+      });
+  }
+}
+
+function removeTodo(key) {
+  firebase
+    .database()
+    .ref('todos/' + key)
+    .remove();
+}
+
+function updateDOM(snapshot) {
+  // Valitse sivulta ul-elementti
+  var todos = document.getElementById('todos');
+  todos.innerHTML = '';
+  snapshot.forEach(function(childSnapshot) {
+    var childData = childSnapshot.val();
+
+    var elem = document.createElement('li');
+    if (childData.done) {
+      elem.className = 'done';
+    }
+
+    elem.setAttribute('data-dbkey', childSnapshot.key);
+    elem.innerHTML = childData.name;
+    todos.insertBefore(elem, todos.firstChild);
+  });
+}
+
+// Firebase-tietokannan asetukset
+var config = {
+  apiKey: 'AIzaSyDZrvIXHig9GDl8cgFtXmTYheOVnnp-dAU',
+  authDomain: 'todo-app-f7a03.firebaseapp.com',
+  databaseURL: 'https://todo-app-f7a03.firebaseio.com',
+  projectId: 'todo-app-f7a03',
+  storageBucket: 'todo-app-f7a03.appspot.com',
+  messagingSenderId: '153458446362',
+};
+// Avaa yhteys Firbase-tietokantaan
+firebase.initializeApp(config);
+
+// Lue tietokannasta tämänhetkinen todo-lista
+readTodos();
+
+document.addEventListener('submit', function(event) {
+  event.preventDefault();
+  var input = document.getElementById('task-input')
+  addNewTodo(input.value);
+  input.value = '';
+});
+
+document.addEventListener('click', function(event) {
+  updateTodo(
+    event.target.dataset.dbkey,
+    event.target.innerHTML,
+    !event.target.classList.contains('done')
+  );
+});
